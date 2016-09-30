27f4cbca7ea08d6379e06c93cf9b931f0be688fa
Marsu5 <marcell.schuh@gmail.com>
2016-09-30 15:15:26 +0200

    Initial commit

---
+++ android/src/hu/tokin/game1/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.tokin.game1;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.tokin.game1.TokinGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TokinGame(), config);
+	}
+}
---
+++ desktop/src/hu/tokin/game1/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.tokin.game1.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.tokin.game1.TokinGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TokinGame(), config);
+	}
+}
