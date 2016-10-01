75c00e1ca673312ee2bef18e8fb88332c792401c
Deni <balint.daniel623@gmail.com>
2016-10-01 19:19:12 +0200

    Alap

--- core/src/hu/tokin/game1/TokinGame.java
+++ core/src/hu/tokin/game1/TokinGame.java
@@ -5,31 +5,39 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class TokinGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Stage stage;
+	Viewport viewport;
+
+	float wigth;
+	float height;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		stage=new Stage();
+		wigth=Gdx.graphics.getWidth();
+		height=Gdx.graphics.getHeight();
+		viewport=new ExtendViewport(wigth,height);
+		Gdx.input.setInputProcessor(stage);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-
-
-		batch.draw(img, 0, 0);
-		batch.end();
+		stage.act(Gdx.graphics.getDeltaTime());
+		stage.draw();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
